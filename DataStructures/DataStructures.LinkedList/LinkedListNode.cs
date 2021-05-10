1e55f3500626cdde243a327ebb176eb20c108398
Dalibor Misic <Dalibor.Misic@endava.com>
2021-05-10 16:26:26 +0200

    Added LinkedListNode and LinkedList class with add, print and reverse methods

---
+++ DataStructures/DataStructures.LinkedList/LinkedListNode.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructures
+{
+    public sealed class LinkedListNode<T>
+    {
+        public T Value { get; set; }
+        public LinkedListNode<T> Next { get; set; }
+
+        public LinkedListNode(T value) => Value = value;
+    }
+}
---
+++ DataStructures/LinkedList.Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using DataStructures.LinkedList;
+using System;
+
+namespace LinkedList.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var linkedList = new LinkedList<int>();
+
+            linkedList.AddToHead(5);
+            linkedList.AddToHead(3);
+            linkedList.AddToHead(1);
+            linkedList.Print();
+
+            linkedList.AddToTail(7);
+            linkedList.AddToTail(9);
+            linkedList.Print();
+
+            linkedList.Reverse();
+            linkedList.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
