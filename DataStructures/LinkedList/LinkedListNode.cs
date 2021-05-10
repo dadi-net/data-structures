a9489a971b8303848831b1c27292f027e07f86d9
Dalibor Misic <Dalibor.Misic@endava.com>
2021-05-10 17:07:54 +0200

    Project reorganization

---
+++ DataStructures/LinkedList/LinkedListNode.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedList
+{
+    public sealed class LinkedListNode<T>
+    {
+        public T Value { get; set; }
+        public LinkedListNode<T> Next { get; set; }
+
+        public LinkedListNode(T value) => Value = value;
+    }
+}
